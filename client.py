6493bedd5dc9c1c11b20cc164abe4ffcc8a26a1d
Beatrix-droid <amilcare8@gmail.com>
2022-01-02 14:51:18 +0000

    completed server script

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import select
 import sys
+from threading import Thread
+#from flask import request
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP_address = str(sys.argv[1])
@@ -8,23 +10,33 @@ PORT = 5008
 
 server.connect(IP_address, PORT)
 
-while True:
-	#maintains lists of possible input streams
-	sockets_lists = [sys.stdin, server]
+def listen_for_messages():
+	while True:
+		#maintains lists of possible input streams
+		sockets_lists = [sys.stdin, server]
 
-	#either the server wants to send a message to be printed on the screen
-	#or the client wants to give manual input to send to other people
+		#either the server wants to send a message to be printed on the screen
+		#or the client wants to give manual input to send to other people
 
-	read_sockets,write_socket, error_socket = select.select(sockets_lists,[],[])
+		read_sockets,write_socket, error_socket = select.select(sockets_lists,[],[])
 
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recieve(1024)
-			print(message)
-		else:
-			message = sys.stdin.readline()
-			server.send(message)
-			sys.stdout.write("You")
-			sys.stdout.write(message)
-			sys.stdout.flush()
-	server.close()
+		for socks in read_sockets:
+			if socks == server:
+				message = socks.recieve(1024)
+				print(message)
+			else:
+				message = sys.stdin.readline()
+				server.send(message)
+				sys.stdout.write(request.form.get("username"))
+				sys.stdout.write(message)
+				sys.stdout.flush()
+
+#daemon thread that listens for messages to this client and prints them
+thread = Thread(target=listen_for_messages())
+
+#thread deamon must end when main thread ends
+thread.daemon = True
+#start thread
+thread.start()
+
+server.close()
--- server.py
+++ server.py
@@ -2,56 +2,82 @@ import socket
 from threading import Thread
 
 HOST = "0.0.0.0"
-PORT = 5008
+# must be a web server ip if run on a web server
+PORT = 55008
 
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(HOST, PORT)
 
-#Making the port a reusable port:
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+# Making the port a reusable port:
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-#Listen for connetctions:
-client_sockets =[]
-s.listen(10)
+# Listen for connections:
+client_sockets = []
+nicknames = []
+# Server listening for incoming connections
+s.listen()
 
 print(f"listening as {HOST}: {PORT}")
+
+# function that sends a message to all clients connected on the server:
+
+
+def broadcast(message):
+	for client in client_sockets:
+		client.send(message)
 
 
 def accept_clients(client):
 
 	""" A function that listens out for messages from the client socket.
-	When a message is recieved, it broadcasts it out to other connected clients"""
+	When a message is received, it broadcasts it out to other connected clients"""
 
 	while True:
 		try:
+			# try to receive a message from the client and if it succeeds broadcast it to the rest of the connected clients
 			message = client.recv(1024).decode()
+			broadcast(message)
+
 		except Exception as e:
-			# the client is no longer connected and must be removed
+			# the client is no longer connected and must be removed. We are going to cut the connection and terminate the loop
 			print(f"Error: {e}")
+			# find the index this particular client has in the client_socket list so that we can remove it
+			index = client_sockets.index(client)
 			client_sockets.remove(client)
+			client.close()
 
-		for client_socket in client_sockets:
-			client_socket.send(message.encode())
-
-while True:
-	# keep listening for new connections all the time
-	client_socket, client_address = s.accept()
-	print(f"{client_address} has conneccted")
-
-	#add new connected client to the connected sockets
-	client_sockets.append(client_socket)
-	#start a new thread that listens for each client's messages
-
-	thread = Thread (target=accept_clients, args=(client_socket,))
-	#make the thread daemon so it ends whenever the main thread ends
-	thread.daemon = True
+			# set the nickname index to be the same as the client index so that we can easily remove the nickname as well without
+			# creating inconsistencies.
+			nickname = nicknames[index]
+			nicknames.remove(nickname)
+			broadcast(f"{nickname} has left the chat".encode("ascii"))
+			break
 
 
-#close client sockets
-for client_socket in client_sockets:
-	client_socket.close()
 
-#close server socket:
-s.close
+def receive():
+	while True:
+		# we will see the client and its Ip address when it connects to the server
+		client, client_address = s.accept()
+		print(f"{client_address} has connected")
 
+		# prompt client to enter a nickname:
+		client.send("NICK".encode("ascii"))
+		nickname = client.recv(1024).decode("ascii")
+
+		# add new connected client to the connected sockets
+		nicknames.append(nickname)
+		client_sockets.append(client)
+		print(f"nickname of client is {nickname}.".encode("ascii"))
+		broadcast(f"{nickname} has joined the chat!".encode("ascii"))
+		client.send("connected to the server".encode("ascii"))
+
+		# start a new thread that listens for each client's messages and handles them at the same time
+		thread = Thread(target=accept_clients, args=(client,))
+		# make the thread daemon such that it ends whenever the main thread ends
+		thread.daemon = True
+		thread.start()
+
+print("server is listening")
+receive()
