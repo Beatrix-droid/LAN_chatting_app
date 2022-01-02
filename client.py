fea117c1f51ee660eec108062219b2c851b29eb9
Beatrix-droid <amilcare8@gmail.com>
2022-01-02 15:16:30 +0000

    minor edits of client script

--- client.py
+++ client.py
@@ -2,21 +2,21 @@ import socket
 import select
 import sys
 from threading import Thread
-#from flask import request
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP_address = str(sys.argv[1])
-PORT = 5008
+PORT = 55008
 
-server.connect(IP_address, PORT)
+server.connect((IP_address, PORT))
 
 def listen_for_messages():
 	while True:
-		#maintains lists of possible input streams
+		# maintains lists of possible input streams
 		sockets_lists = [sys.stdin, server]
 
-		#either the server wants to send a message to be printed on the screen
-		#or the client wants to give manual input to send to other people
+		# either the server wants to send a message to be printed on the screen
+		# or the client wants to give manual input to send to other people
 
 		read_sockets,write_socket, error_socket = select.select(sockets_lists,[],[])
 
@@ -27,16 +27,16 @@ def listen_for_messages():
 			else:
 				message = sys.stdin.readline()
 				server.send(message)
-				sys.stdout.write(request.form.get("username"))
+				#s ys.stdout.write(request.form.get("username"))
 				sys.stdout.write(message)
 				sys.stdout.flush()
 
-#daemon thread that listens for messages to this client and prints them
-thread = Thread(target=listen_for_messages())
+			# daemon thread that listens for messages to this client and prints them
+			thread = Thread(target=listen_for_messages())
 
-#thread deamon must end when main thread ends
-thread.daemon = True
-#start thread
-thread.start()
+			# thread deamon must end when main thread ends
+			thread.daemon = True
+			# start thread
+			thread.start()
 
-server.close()
+			server.close()
