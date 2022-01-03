2db6100eeb15f629aa6958f6bd759e74ccf141e1
Beatrix-droid <amilcare8@gmail.com>
2022-01-03 13:03:39 +0000

    added function that listens for messages in client.py

--- client.py
+++ client.py
@@ -4,32 +4,36 @@ import sys
 from threading import Thread
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP_address = str(sys.argv[1])
 PORT = 55008
+client.connect((IP_address, PORT))
 
-server.connect((IP_address, PORT))
+nickname = input("choose a nickname")
 
 def listen_for_messages():
 	while True:
-		# maintains lists of possible input streams
-		sockets_lists = [sys.stdin, server]
-
 		# either the server wants to send a message to be printed on the screen
 		# or the client wants to give manual input to send to other people
+		# maintains lists of possible input streams
+		sockets_lists = [sys.stdin, client]
+
 
 		read_sockets,write_socket, error_socket = select.select(sockets_lists,[],[])
-
+		# receiving messages from the server
 		for socks in read_sockets:
-			if socks == server:
-				message = socks.recieve(1024)
-				print(message)
-			else:
-				message = sys.stdin.readline()
-				server.send(message)
-				#s ys.stdout.write(request.form.get("username"))
-				sys.stdout.write(message)
-				sys.stdout.flush()
+			try:
+				message = socks.recieve(1024).decode("ascii")
+				if message == "NICK":
+					client.send(nickname.encode("ascii"))
+				else:
+					print(message)
+			except:
+				# if an error has occured we close the connection
+				print("An Error has occurred!")
+				client.close()
+				break
+
 
 			# daemon thread that listens for messages to this client and prints them
 			thread = Thread(target=listen_for_messages())
@@ -39,4 +43,3 @@ def listen_for_messages():
 			# start thread
 			thread.start()
 
-			server.close()
