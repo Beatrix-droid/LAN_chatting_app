e7f249cbd9d9d594aed2d1dc2f241454602fa57f
Beatrice Federici <beatrice.federici123@gmail.com>
2022-01-14 11:35:01 +0000

    fixed session username bug

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, flash, session, redirect
+from flask_session import Session
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.static_folder = "static"
 app.secret_key = "My_secret_key"
 app.config["SECRET KEY"] = "another_secret!"
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 socketio = SocketIO(app)
 
 
@@ -15,9 +18,9 @@ def login_form():
 
 @app.route('/chat-page', methods=["POST"])
 def chat_page():
-	username = request.form.get("user_name")
-	session["user"] = username
-	return render_template('chat-page.html', Uname=username)
+    username = request.form.get("user_name")
+    session["user"] = username
+    return render_template('chat-page.html', Uname=username)
 
 
 @app.route("/logout")
@@ -27,13 +30,11 @@ def logout():
 	return redirect("/")
 
 
-
 # function to send messages to the entire group
 @socketio.on("message")
 def handle_message(msg):
 	print("message: " + msg)
 	send(msg, broadcast=True)
-
 
 
 if __name__ == "__main__":
